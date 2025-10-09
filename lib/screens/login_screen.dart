32904b49d7b9b4376a47c42e280179aa3895448b
Eduardo450 <146303031+Eduardo450@users.noreply.github.com>
2025-10-08 23:37:24 -0600

    feat(login): focus-based bear reactions (email down, password up)

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -19,151 +19,185 @@ class _LoginScreenState extends State<LoginScreen> {
   SMITrigger? trigSuccess; //Se emociona
   SMITrigger? trigFail; //Se pone triste
 
+  // 1) FocusNode
+  final emailFocus = FocusNode();
+  final passFocus = FocusNode();
+
+  //2) Listeners (Oyentes)
+  @override
+  void initState() {
+    super.initState();
+    emailFocus.addListener(() {
+    if (emailFocus.hasFocus) {
+      //Manos abajo en email
+      isHandsUp?.change(false);
+      }
+    });
+    passFocus.addListener((){
+      //Manos arriba en Password
+      isHandsUp?.change(passFocus.hasFocus);
+    });
+  }
+
 
   @override
   Widget build(BuildContext context) {
     //Obtener el tamaño de la pantalla del dispositivo
     final Size size = MediaQuery.of(context).size;
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       //Evita el notch o la cámara frontal
       body: SafeArea(
-        child: Padding(
-          //Espaciado
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            children: [
-              SizedBox(
-                width: size.width,
-                height: 200,
-                child: RiveAnimation.asset(
-                  'assets/animated_login_character.riv',
-                  stateMachines: ["Login Machine"],
-                  //Al iniciarse
-                  onInit: (artboard){
-                    controller =
-                    StateMachineController.fromArtboard(
-                      artboard,
-                      "Login Machine",
-                    );
-                    //Verificar que inició bien
-                    if(controller == null) return;
-                    artboard.addController(controller!);
-                    isChecking  = controller!.findSMI('isChecking');
-                    isHandsUp   = controller!.findSMI('isHandsUp');
-                    trigSuccess = controller!.findSMI('trigSuccess');
-                    trigFail    = controller!.findSMI('trigFail');
-                  },
-                  ),
-              ),
-              //Espacio entre el oso y el texto email
-              const SizedBox(height: 10,),
-              //Campo de texto del email
-              TextField(
-                onChanged: (value) {
-                  if (isHandsUp != null){
-                    //Reafirma no taparse los ojos al escribir el email
-                    isHandsUp!.change(false);
-                  }
-                  if (isChecking == null) return;
-                  //Activa el modo
-                  isChecking!.change(true);
-                },
-                //Para que aparezca el @ en Móviles UI/UX
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  prefixIcon: const Icon(Icons.mail),
-                  border: OutlineInputBorder(
-                    //esquinas redondeadas
-                    borderRadius: BorderRadius.circular(12),
-                  )
-                ),
-              ),
-              SizedBox(height: 12,),
-                TextField(
-                  onChanged: (value) {
-                  if (isChecking != null){
-                    //Reafirma no taparse los ojos al escribir el email
-                    isChecking!.change(false);
-                  }
-                  if (isHandsUp == null) return;
-                  //Activa el modo
-                  isHandsUp!.change(true);
-                },
-                  obscureText: obscurePassword,
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    prefixIcon: const Icon(Icons.lock),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    suffixIcon: IconButton(
-                      icon: Icon(
-                        obscurePassword ? Icons.visibility : Icons.visibility_off,
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          obscurePassword = !obscurePassword;
-                        });
-                      },
-                    ),
-                  ),
-                ),
-                const SizedBox (height: 10),
-                //Texto Olvidé la contraseña como botón
+        child: SingleChildScrollView(
+          child: Padding(
+            //Espaciado
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
                 SizedBox(
                   width: size.width,
-                  child: Align(
-                    alignment: Alignment.centerRight,
-                    child: TextButton(
-                      onPressed: () {},
-                      child: const Text(
-                        'Forgot your password?',
-                        style: TextStyle(
-                          decoration: TextDecoration.underline,
-                          color: Colors.black,
+                  height: 200,
+                  child: RiveAnimation.asset(
+                    'assets/animated_login_character.riv',
+                    stateMachines: ["Login Machine"],
+                    //Al iniciarse
+                    onInit: (artboard){
+                      controller =
+                      StateMachineController.fromArtboard(
+                        artboard,
+                        "Login Machine",
+                      );
+                      //Verificar que inició bien
+                      if(controller == null) return;
+                      artboard.addController(controller!);
+                      isChecking  = controller!.findSMI('isChecking');
+                      isHandsUp   = controller!.findSMI('isHandsUp');
+                      trigSuccess = controller!.findSMI('trigSuccess');
+                      trigFail    = controller!.findSMI('trigFail');
+                    },
+                    ),
+                ),
+                //Espacio entre el oso y el texto email
+                const SizedBox(height: 10,),
+                //Campo de texto del email
+                TextField(
+                  //Asignas el FocusNode al Textf¿Field
+                  focusNode: emailFocus,
+                  onChanged: (value) {
+                    if (isHandsUp != null){
+                      //Reafirma no taparse los ojos al escribir el email
+                      //isHandsUp!.change(false);
+                    }
+                    if (isChecking == null) return;
+                    //Activa el modo
+                    isChecking!.change(true);
+                  },
+                  //Para que aparezca el @ en Móviles UI/UX
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    prefixIcon: const Icon(Icons.mail),
+                    border: OutlineInputBorder(
+                      //esquinas redondeadas
+                      borderRadius: BorderRadius.circular(12),
+                    )
+                  ),
+                ),
+                SizedBox(height: 12,),
+                  TextField(
+                    //Asignas el FocusNode al Textf¿Field
+                    focusNode: passFocus,
+                    onChanged: (value) {
+                    if (isChecking != null){
+                      //Reafirma no taparse los ojos al escribir el email
+                      //isChecking!.change(false);
+                    }
+                    if (isHandsUp == null) return;
+                    //Activa el modo
+                    isHandsUp!.change(true);
+                  },
+                    obscureText: obscurePassword,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      prefixIcon: const Icon(Icons.lock),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          obscurePassword ? Icons.visibility : Icons.visibility_off,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            obscurePassword = !obscurePassword;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                  const SizedBox (height: 10),
+                  //Texto Olvidé la contraseña como botón
+                  SizedBox(
+                    width: size.width,
+                    child: Align(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                        onPressed: () {},
+                        child: const Text(
+                          'Forgot your password?',
+                          style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            color: Colors.black,
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-                //Botón de Login
-                const SizedBox(height: 10),
-                //Botón estilo Android
-                MaterialButton(
-                  minWidth: size.width,
-                  height: 50,
-                  color: Colors.purple,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12)
+                  //Botón de Login
+                  const SizedBox(height: 10),
+                  //Botón estilo Android
+                  MaterialButton(
+                    minWidth: size.width,
+                    height: 50,
+                    color: Colors.purple,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12)
+                    ),
+                    onPressed: (){},
+                    child: Text(
+                      'Login',
+                      style: TextStyle(color: Colors.white)),
                   ),
-                  onPressed: (){},
-                  child: Text(
-                    'Login',
-                    style: TextStyle(color: Colors.white)),
-                ),
-                const SizedBox(height: 10,),
-                SizedBox(
-                  width: size.width,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text("New here?"),
-                      TextButton(onPressed: (){}, child: const Text(
-                        "Register",
-                        style: TextStyle(
-                          color: Colors.black,
-                          //Negritas
-                          fontWeight: FontWeight.bold,
-                          //Subrayado
-                          decoration: TextDecoration.underline,
-                        )))
-                    ],)
-                )
-            ],
+                  const SizedBox(height: 10,),
+                  SizedBox(
+                    width: size.width,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text("New here?"),
+                        TextButton(onPressed: (){}, child: const Text(
+                          "Register",
+                          style: TextStyle(
+                            color: Colors.black,
+                            //Negritas
+                            fontWeight: FontWeight.bold,
+                            //Subrayado
+                            decoration: TextDecoration.underline,
+                          )))
+                      ],)
+                  )
+              ],
+            ),
           ),
         )
       )
     );
+  }
+  //Liberación de recursos/Limpieza de Focus
+  @override
+  void dispose() {
+    emailFocus.dispose();
+    passFocus.dispose();
+    super.dispose();
   }
 }
